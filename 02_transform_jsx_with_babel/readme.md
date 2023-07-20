f430b998ede8293b7266d3cd37604729e996a07d
ashisingbam <ashisinghbam@example.com>
2023-07-20 21:57:37 +0530

    transform jsx with babel complete

---
+++ 02_transform_jsx_with_babel/readme.md
@@ -0,0 +1,19 @@
+## Transform JSX with Babel
+
+📖 Chapters
+- 00:00 - Introduction
+- 00:31 - Quick Recap
+- 01:18 - Why do we need Babel?
+- 02:18 - What is Babel?
+- 03:09 - Let's use Babel CDN for now
+- 04:10 - Understand the "type" attribute in "script" tag
+- 14:59 - What is a Source Map?
+- 16:30 - Let's debug the code inside 'babel.js' to understand things in depth
+- 28:39 - How to write JavaScript inside JSX?
+- 30:47 - Settings for Emmet Suggestion
+- 32:17 - Few values that React doesn't show on the page!
+- 36:10 - Why we need to use double curly brackets "{{}}" for style tag?
+- 36:40 - What is JSX?
+- 37:50 - Babel Setup
+- 45:20 - Next Video - Source Maps
+- 46:20 - Conclusion
---
+++ 02_transform_jsx_with_babel/script.js
@@ -0,0 +1,31 @@
+// const container = (
+//   <div className="container" id="subtitle">
+//     <section className="sec">
+//       <p>React is super awesome</p>
+//       <img
+//         src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+//         style={{
+//           width: 200,
+//           backgroundColor: 'teal',
+//           padding: 16,
+//           borderRadius: 8,
+//         }}
+//       />
+//     </section>
+//     <form>
+//       <div className="input-group">
+//         <label htmlFor="username">Username:</label>
+//         <input id="username" />
+//       </div>
+//       <div className="password-group">
+//         <label htmlFor="password">Password:</label>
+//         <input id="password" type="password" />
+//       </div>
+//     </form>
+//   </div>
+// )
+
+const user = 'Ashi'
+const h2 = <h2>Hello {5+9}</h2>
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+root.render(h2)
